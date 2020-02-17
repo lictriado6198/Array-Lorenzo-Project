493672ed6c24726e501d0e36cd573625b018c0e2
PC <PC@PC-PC>
2020-02-18 00:35:47 +0800

    array lorenzo project

---
+++ src/array/lorenzo/project/ArrayLorenzoProject.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package array.lorenzo.project;
+import java.util.Scanner;
+/**
+ *
+ * @author PC
+ */
+public class ArrayLorenzoProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    int[][] worldPopulation = {
+                               {1000,1500,2000,2500,3900,3232},
+                               {2200,2500,1200,1400},
+                               {3000,3300,2323,6623,3232},
+   
+                            };
+        
+     System.out.println("worldPopulation is:"+worldPopulation+"and length is :"+worldPopulation.length);     
+     System.out.println("worldPopulation[0] is:"+worldPopulation[0]+"and length is :"+worldPopulation[0].length);   
+     System.out.println("worldPopulation[1] is:"+worldPopulation[1]+"and length is :"+worldPopulation[1].length);  
+     System.out.println("worldPopulation[2] is:"+worldPopulation[2]+"and length is :"+worldPopulation[2].length);  
+ 
+     System.out.println("worldPopulation[0][0]:"+worldPopulation[0][0]);
+     System.out.println("worldPopulation[0][1]:"+worldPopulation[0][1]); 
+     System.out.println("worldPopulation[0][2]:"+worldPopulation[0][2]);
+     System.out.println("worldPopulation[0][3]:"+worldPopulation[0][3]);
+     System.out.println("worldPopulation[0][4]:"+worldPopulation[0][4]);
+     System.out.println("worldPopulation[0][5]:"+worldPopulation[0][5]);
+     
+ 
+     System.out.println("worldPopulation[1][0]:"+worldPopulation[1][0]);
+     System.out.println("worldPopulation[1][1]:"+worldPopulation[1][1]);
+     System.out.println("worldPopulation[1][2]:"+worldPopulation[1][2]);
+     System.out.println("worldPopulation[1][3]:"+worldPopulation[1][3]);
+     
+     System.out.println("worldPopulation[2][0]:"+worldPopulation[2][0]); 
+     System.out.println("worldPopulation[2][1]:"+worldPopulation[2][1]);
+     System.out.println("worldPopulation[2][2]:"+worldPopulation[2][2]);
+     System.out.println("worldPopulation[2][3]:"+worldPopulation[2][3]);
+     System.out.println("worldPopulation[2][4]:"+worldPopulation[2][4]);
+     
+     worldPopulation[0][0]=8000;
+     worldPopulation[0][1]=8999;
+     worldPopulation[0][2]=7878;
+     worldPopulation[0][3]=9990;
+     worldPopulation[0][4]=9898;
+     worldPopulation[0][5]=6777;
+     
+     worldPopulation[1][0]=7777;
+     worldPopulation[1][1]=6666;
+     worldPopulation[1][2]=6868;
+     worldPopulation[1][3]=9222;
+     
+     worldPopulation[2][0]=10323;
+     worldPopulation[2][1]=10500;
+     worldPopulation[2][2]=50500;
+     worldPopulation[2][3]=26666;
+     worldPopulation[2][4]=89786;
+    
+     System.out.println("after 2 years RE reading ");
+     System.out.println("just watch");
+     System.out.println(" L  ");      
+     System.out.println(" 0  ");
+     System.out.println(" V ");
+     System.out.println(" E");
+     System.out.println("");
+     
+     System.out.println("worldPopulation[0][0]:"+worldPopulation[0][0]);
+     System.out.println("worldPopulation[0][1]:"+worldPopulation[0][1]); 
+     System.out.println("worldPopulation[0][2]:"+worldPopulation[0][2]);
+     System.out.println("worldPopulation[0][3]:"+worldPopulation[0][3]);
+     System.out.println("worldPopulation[0][4]:"+worldPopulation[0][4]);
+     System.out.println("worldPopulation[0][5]:"+worldPopulation[0][5]);
+     
+     System.out.println("worldPopulation[1][0]:"+worldPopulation[1][0]);
+     System.out.println("worldPopulation[1][1]:"+worldPopulation[1][1]);
+     System.out.println("worldPopulation[1][2]:"+worldPopulation[1][2]);
+     System.out.println("worldPopulation[1][3]:"+worldPopulation[1][3]);
+     
+     System.out.println("worldPopulation[2][0]:"+worldPopulation[2][0]); 
+     System.out.println("worldPopulation[2][1]:"+worldPopulation[2][1]);
+     System.out.println("worldPopulation[2][2]:"+worldPopulation[2][2]);
+     System.out.println("worldPopulation[2][3]:"+worldPopulation[2][3]);
+     System.out.println("worldPopulation[2][4]:"+worldPopulation[2][4]);
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+    
+}
